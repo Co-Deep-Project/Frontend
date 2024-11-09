2c8be3564859ff2e2143bf65270f077eb44466c4
sodaaw <ghk132435@gmail.com>
2024-11-09 09:20:27 +0900

    updated css

--- co-deep-frontend(Final)/src/gahyun/Desktop.js
+++ co-deep-frontend(Final)/src/gahyun/Desktop.js
@@ -1,23 +1,18 @@
 import React from "react";
 import "./style.css";
 import "./styleguide.css";
-
-import frame2 from "../assets/frame-2.svg";
-import frame from "../assets/frame.svg";
-import group from "../assets/group.png";
-import tokyoBrowserWindowWithEmoticonLikesAndStarsAround2 from "../assets/tokyo-browser-window-with-emoticon-likes-and-stars-around-2.png";
-import tokyoMagnifierWebSearchWithElements2 from "../assets/tokyo-magnifier-web-search-with-elements-2.png";
-import image from "../assets/Button.png"; 
 import arrow1 from "../assets/arrow-1.svg";
 import arrow2 from "../assets/arrow-1-2.svg";
-import logo from "../assets/polilogo.png"; // 로고 이미지 import
+import logo from "../assets/polilogo.png"; 
 
 const Desktop = () => {
   return (
     <div className="desktop">
+      
+      {/* header section */}
       <div className="landing-page">
         <div className="navigation-bar">
-          <div className="POLITRACK-er">
+          <div className="POLITRACKER">
             <img
               src={logo}
               alt="POLITRACKER 로고"
@@ -25,44 +20,30 @@ const Desktop = () => {
             />
           </div>
         </div>
+
         <header className="header">
           <p className="p">
+            <br />
             <br />
             <br />
             객관적 정보, 주체적 판단
             <br />
             청년을 위한 정치 플랫폼
           </p>
-          <div className="illustration">
-            <div className="overlap">
-              <img className="frame" src={frame} alt="Frame" />
-              <div className="overlap-group">
-                <img className="img" src={frame2} alt="Frame 2" />
-                <div className="ellipse"></div>
-                <div className="ellipse-2"></div>
-                <div className="ellipse-3"></div>
-                <img className="group" src={group} alt="Group" />
-              </div>
-              <div className="ellipse-4"></div>
-              <div className="ellipse-5"></div>
-              <div className="ellipse-6"></div>
-            </div>
-          </div>
         </header>
       </div>
+
+      {/* Intro Section */}
       <div className="heading-subheading">
-        <div className="heading">
-          <div className="label">
-            <div className="text-wrapper">서비스</div>
-          </div>
-        </div>
         <p className="div">
           정치가 어렵게 느껴지시나요?
           <br />
           폴리트래커로 중요한 사안과 정보를 한눈에, 쉽게 확인하세요!
         </p>
       </div>
+
       <div className="overlap-2">
+        {/* Cards Section */}
         <div className="card">
           <div className="heading-and-link">
             <div className="heading">
@@ -73,21 +54,17 @@ const Desktop = () => {
                 <div className="label-3">분석하기</div>
               </div>
             </div>
+            <br></br>
+            <br></br>
             <div className="link">
               <div className="icon">
                 <img className="arrow" src={arrow1} alt="Arrow" />
               </div>
+              <br></br>
               <div className="label-4">Learn more</div>
             </div>
           </div>
-          <div className="tokyo-browser-window-wrapper">
-            <img
-              className="tokyo-browser-window"
-              src={tokyoBrowserWindowWithEmoticonLikesAndStarsAround2}
-              style={{ width: "100px", height: "auto" }}
-              alt="Browser Window"
-            />
-          </div>
+          <div className="tokyo-browser-window-wrapper"></div>
         </div>
         <div className="cards-section">
           <div className="card-2">
@@ -100,21 +77,17 @@ const Desktop = () => {
                   <div className="label-3">트래킹하기</div>
                 </div>
               </div>
+              <br></br>
+              <br></br>
               <div className="link">
                 <div className="arrow-wrapper">
                   <img className="arrow" src={arrow2} alt="Arrow 2" />
                 </div>
+                <br></br>
                 <div className="label-5">Learn more</div>
               </div>
             </div>
-            <div className="tokyo-magnifier-web-wrapper">
-              <img
-                className="tokyo-magnifier-web"
-                src={tokyoMagnifierWebSearchWithElements2}
-                alt="Magnifier Web"
-                style={{ width: "100px", height: "auto" }}
-              />
-            </div>
+            <div className="tokyo-magnifier-web-wrapper"></div>
           </div>
         </div>
         <div className="card-3">
@@ -127,16 +100,21 @@ const Desktop = () => {
                 <div className="label-3">알아보기</div>
               </div>
             </div>
+            <br></br>
+            <br></br>
             <div className="link">
               <div className="arrow-wrapper">
                 <img className="arrow" src={arrow1} alt="Arrow" />
               </div>
+              <br></br>
               <div className="label-5">Learn more</div>
             </div>
           </div>
           <div className="illustration-2"></div>
         </div>
       </div>
+
+      <br />
       <p className="element-trackerskku-g">
         성균관대학교 트래커스꾸
         <br />
