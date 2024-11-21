92cc4f8c31c5683d61dd5166d9c4d3e9b340fb45
seoin2744 <seoin9398@gmail.com>
2024-11-21 18:28:12 +0900

    css 디테일 수정

--- co-deep-frontend(Final)/src/seoin/Seoin.jsx
+++ co-deep-frontend(Final)/src/seoin/Seoin.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./style.css";
+import "./seoin_style.css";
 
 const Seoin = () => {
   const navigate = useNavigate();
