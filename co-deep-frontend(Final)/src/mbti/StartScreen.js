db4656814246fccfe5b7ec4c1c3bf4cb0640eb9a
SyraYunji <syralee1004@gmail.com>
2025-01-01 21:33:52 +0700

    start screen 수정

--- co-deep-frontend(Final)/src/mbti/StartScreen.js
+++ co-deep-frontend(Final)/src/mbti/StartScreen.js
@@ -56,7 +56,7 @@ const StartScreen = () => {
           </button>
         </div>
       </div>
-      <footer className="footer">
+      <footer className="start-footer">
         <p>성균관대학교 트래커스꾸<br />서울특별시 종로구 성균관로 25-2<br />trackerskku@g.skku.edu</p>
       </footer>
     </div>
